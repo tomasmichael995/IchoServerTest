caf0a1b5ed03f69bce84b6b775ac313c8d76d650
Tomas Michael <tomasmichael995@gmail.com>
2019-03-15 00:31:01 +0200

    Created MainActivity.enableBluetooth()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
@@ -1,6 +1,7 @@
 package gr.kalymnos.sk3m3l10.ichoremotetest;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 13715;
+
     private Toolbar toolbar;
     private ImageView bluetoothImage;
     private BluetoothAdapter bluetoothAdapter;
@@ -19,19 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initFields();
         exitIfBluetoothNotSupported();
-    }
-
-    private void exitIfBluetoothNotSupported() {
-        if (bluetoothAdapter == null){
-            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
-            finish();
-        }
+        enableBluetooth();
     }
 
     private void initFields() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         toolbar = findViewById(R.id.toolBar);
         bluetoothImage = findViewById(R.id.bluetooth_image);
+    }
+
+    private void exitIfBluetoothNotSupported() {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private void enableBluetooth() {
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 
     public void onSendClick(View view) {
