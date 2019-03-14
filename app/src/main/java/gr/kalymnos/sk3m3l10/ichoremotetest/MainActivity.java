802f33fe014627e94970875d51ab5cf7f7290e8e
Tomas Michael <tomasmichael995@gmail.com>
2019-03-15 00:19:38 +0200

    Initiazing fields in MainActivity

--- app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
@@ -1,18 +1,31 @@
 package gr.kalymnos.sk3m3l10.ichoremotetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ImageView bluetoothImage;
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFields();
     }
 
-    public void onSendClick(View view){
+    private void initFields() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        toolbar = findViewById(R.id.toolBar);
+        bluetoothImage = findViewById(R.id.bluetooth_image);
+    }
+
+    public void onSendClick(View view) {
 
     }
 }
