a8b9d65f368184b2976a12035b4ff64e35ab0845
Tomas Michael <tomasmichael995@gmail.com>
2019-03-15 12:13:53 +0200

    Initializing BluetoothAdapter and stateReceiver in MainActivity

--- app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/MainActivity.java
@@ -2,6 +2,8 @@ package gr.kalymnos.sk3m3l10.ichoremotetest;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -10,7 +12,12 @@ import android.widget.Toast;
 import gr.kalymnos.sk3m3l10.ichoremotetest.mvc_views.MainScreenViewMvc;
 import gr.kalymnos.sk3m3l10.ichoremotetest.mvc_views.MainScreenViewMvcImpl;
 
-public class MainActivity extends AppCompatActivity implements MainScreenViewMvc.OnSendClickListener{
+import static android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED;
+import static android.bluetooth.BluetoothAdapter.EXTRA_STATE;
+import static android.bluetooth.BluetoothAdapter.STATE_OFF;
+import static android.bluetooth.BluetoothAdapter.STATE_ON;
+
+public class MainActivity extends AppCompatActivity implements MainScreenViewMvc.OnSendClickListener {
     private static final int REQUEST_ENABLE_BT = 13715;
 
     private MainScreenViewMvc viewMvc;
@@ -20,18 +27,47 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initViewMvc();
+        initFields();
         setupUi();
     }
 
+    private void initFields() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        stateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case ACTION_STATE_CHANGED:
+                        int state = intent.getIntExtra(EXTRA_STATE, STATE_OFF);
+                        setUiFrom(state);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown action.");
+                }
+            }
+
+            private void setUiFrom(int state) {
+                if (state == STATE_ON) {
+                    viewMvc.showBluetooth();
+                    viewMvc.setToolbarSubtitle("Bluetooth enabled.");
+                } else {
+                    viewMvc.showBluetoothDisabled();
+                    viewMvc.setToolbarSubtitle("Bluetooth disabled.");
+                }
+            }
+        };
+        initViewMvc();
+    }
+
     private void initViewMvc() {
-        viewMvc = new MainScreenViewMvcImpl(LayoutInflater.from(this),null);
+        viewMvc = new MainScreenViewMvcImpl(LayoutInflater.from(this), null);
         viewMvc.setOnSendClickListener(this);
     }
 
     private void setupUi() {
         setContentView(viewMvc.getRootView());
         setSupportActionBar(viewMvc.getToolbar());
+        viewMvc.setToolbarTitle(getString(R.string.app_name));
     }
 
     @Override
