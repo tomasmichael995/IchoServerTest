8f4bb3a6e90ae5713ccdcbc53ea6128e1a4fc7b5
Tomas Michael <tomasmichael995@gmail.com>
2019-03-18 16:10:08 +0200

    ServerStatus is a class

--- app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/Bluetooth/ServerStatus.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ichoremotetest/Bluetooth/ServerStatus.java
@@ -1,5 +1,10 @@
 package gr.kalymnos.sk3m3l10.ichoremotetest.Bluetooth;
 
-public enum ServerStatus {
-    CONNECTED, DISCONNECTED, CONNECTION_ERROR, DISSCONECTION_ERROR
+public final class ServerStatus {
+    public static final int CONNECTED = 1;
+    public static final int DISSCONNECTED = 2;
+    public static final int ERROR = 3;
+
+    private ServerStatus() {
+    }
 }
